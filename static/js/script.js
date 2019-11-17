0b73595b23d3ed6467e7fa19be2c0bd252725de0
Ubuntu <ubuntu@ip-172-31-83-209.ec2.internal>
2019-11-17 17:48:51 +0000

    Add constants to target each classname in are being used in javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
-const goBack = document.getElementsByClassName("go-back")
+const goBack = document.getElementsByClassName("go-back");
+const buttonCollapse = document.getElementsByClassName("button-collapse");
+const navbarContainer = document.getElementsByClassName("navbar-container");
+const recipeLogoImage = document.getElementsByClassName("recipe-logo-image");
+const toTop = document.getElementsByClassName("to-top");
+
 /* 
 Whatever code is written inside the JQuery ready method will run once the page Document Object Modal (DOM)
 is ready to execute JavaScript code.
@@ -6,30 +11,30 @@ is ready to execute JavaScript code.
 
 $(document).ready(() => {
     // This is the jquery that gives the mobile burger icon the ability to toggle the mobile sidedrawer
-    $('.button-collapse').sideNav();
+    $(buttonCollapse).sideNav();
     $('input#input_text, textarea#textarea2').characterCounter();
-    // Will check to see if the window has been scrolled 50px from the top
+    // Will check to see if the window has been scrolled 50px from the top.
     $(window).scroll(() => {
         const joinedNATransparent = ['N/A', 'transparent'].join(' ');
         if ($(document).scrollTop() > 50) {
-            $(".navbar-container").removeClass(joinedNATransparent).css('background-color', '#000000a3');
-            $(".recipe-logo-image").css({ 'width': '66px', 'margin-top': '0px' });
-            $('.to-top').css('opacity', '1');
+            $(navbarContainer).removeClass(joinedNATransparent).css('background-color', '#000000a3');
+            $(recipeLogoImage).css({ 'width': '66px', 'margin-top': '0px' });
+            $(toTop).css('opacity', '1');
         }
         else {
-            $(".navbar-container").addClass(joinedNATransparent);
-            $(".recipe-logo-image").css({ 'width': '95px', 'margin-top': '5px' });
-            $('.to-top').css('opacity', '0');
+            $(navbarContainer).addClass(joinedNATransparent);
+            $(recipeLogoImage).css({ 'width': '95px', 'margin-top': '5px' });
+            $(toTop).css('opacity', '0');
         }
     });
-    // Will remove active class and add new active class depending on which is matched with the pathname
+    // Will remove active class and add new active class depending on which is matched with the pathname.
     $('li.active').removeClass('active');
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
     
-    
+    // Will take the user back to the previous page on click.
     $(goBack).on("click", () => {
         window.history.back();
-    })
+    });
 });
 
 
