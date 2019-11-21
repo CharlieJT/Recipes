3aa76daa7aa6be7339c95dd09228a9c7491d1e6e
Ubuntu <ubuntu@ip-172-31-83-209.ec2.internal>
2019-11-21 21:41:31 +0000

    Created feature for creating a recipe

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_pymongo import PyMongo, DESCENDING
 from bson.objectid import ObjectId
-from forms import CreateRecipeForm
+
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ def index():
     return render_template('index.html', title="Home", recipes=recipes)
     
     
-@app.route('/recipe-listing')
+@app.route('/recipe_listing')
 def recipe_listing():
     """This shows a list of all of the recipes"""
     recipes=mongo.db.recipes.find()
@@ -28,7 +28,7 @@ def recipe_listing():
     
 @app.route('/recipe/<recipe_id>')
 def recipe(recipe_id):
-    """Shows all fields in recipes & increments the views by 1"""
+    """This shows all fields in recipes & increments the views by 1"""
     mongo.db.recipes.find_one_and_update(
         {'_id': ObjectId(recipe_id)},
         {'$inc': {'views': 1}}
@@ -37,23 +37,21 @@ def recipe(recipe_id):
     recipe = mongo.db.recipes.find_one_or_404({'_id': ObjectId(recipe_id)})
     return render_template('recipe.html', recipe=recipe)
     
-@app.route('/create_recipe', methods=['GET', 'POST'])
-def create_recipe():
-    create_recipe_form = CreateRecipeForm(request.form)
     
+@app.route('/create_recipe')
+def create_recipe():
+    """This shows a list of all of the recipes"""
     recipes_db = mongo.db.recipes
-    # insert the new recipe
-    recipes_db.insert_one({
-        'recipe_name': request.form['recipe_name'],
-        'recipe_description': request.form['recipe_description'],
-        'recipe_ingredients': request.form['recipe_ingredients'],
-        'recipe_instrictions': request.form['recipe_instrictions'],
-        'recipe_image_url': request.form['recipe_image_url'],
-        'views': 0
-    })
-    return redirect(url_for('index', title='New recipe has been created'))
-    return render_template('create-recipe.html', title="Create Recipe")
-
+    # This is insert a new value for each field
+    return render_template('create-recipe.html')
+    
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    """This will add each field to the database"""
+    recipe_db = mongo.db.recipes
+    recipe_db.insert_one(request.form.to_dict())
+    return redirect(url_for('recipe_listing'))
+    
   
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
